ff8046526d8c73e224b094944ce8287214404b01
LeoEatle <liuyitao811@hotmail.com>
2018-10-09 12:50:15 +0800

    fix: 修改shell

--- weixin-robot.sh
+++ weixin-robot.sh
@@ -4,6 +4,6 @@
 if [ ! -n "$1" ] ;then
 	echo "you have not input any word!"
 elif [ $1 -eq 0 ];then
-	curl -X POST -H 'Content-type: application/json' --data '{"msgtype":"markdown","markdown":{"content":"builder-webpack4测试失败，[查看状态](https://travis-ci.com/feflow/builder-webpack4)"}}' https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$robot_key
+	curl -X POST -H 'Content-type: application/json' --data '{"msgtype":"markdown","markdown":{"content":"builder-webpack4测试失败，[查看状态](https://travis-ci.com/feflow/builder-webpack4)"}}' https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${ROBOT_KEY}
 elif [ $1 -eq 1 ];then
-	curl -X POST -H 'Content-type: application/json' --data '{"msgtype":"markdown","markdown":{"content":"builder-webpack4测试成功，[查看状态](https://travis-ci.com/feflow/builder-webpack4)"}}' https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$robot_key
+	curl -X POST -H 'Content-type: application/json' --data '{"msgtype":"markdown","markdown":{"content":"builder-webpack4测试成功，[查看状态](https://travis-ci.com/feflow/builder-webpack4)"}}' https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${ROBOT_KEY}
